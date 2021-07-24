8b4acef13b27413bca9217483348d62cac9aa025
disgustipated <disgustipated@erasethis.net>
2021-07-24 09:12:40 -0400

    Remove web response on mqtt publish

--- garden_8266_controller_code/MQTT.ino
+++ garden_8266_controller_code/MQTT.ino
@@ -37,16 +37,13 @@ void reconnect() {
   }
 }
 
-void publishMessage(String mqtttopic, JsonObject message, bool webResponse=false){
+void publishMessage(String mqtttopic, JsonObject message){
   checkMQTT();
   char buffer[512];
   size_t n = serializeJson(message, buffer);
   Serial.println(buffer);
   if(client.publish(toCharArray(mqtttopic), toCharArray(buffer), (size_t)n)){
     dataSend();
-    if(webResponse){
-      server.send(200,"text/plain",buffer);
-    }
   } else {
     MQTTError();
   }
